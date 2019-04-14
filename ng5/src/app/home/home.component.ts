5ef23fb3974da85c327948adcccc620e334fed84
Salv95-2 <salvramirez95@gmail.com>
2019-04-13 20:14:17 -0700

    Adding data binding example

--- ng5/src/app/home/home.component.ts
+++ ng5/src/app/home/home.component.ts
@@ -8,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   itemCount: number = 4;
-  btnText: String = 'Add an item';
+  btnText: string = 'Add an item';
+  goalText: string = 'My first life goal';
 
   constructor() { }
 
